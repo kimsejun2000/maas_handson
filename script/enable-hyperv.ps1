8b465bd58a2e4082575fdeb84357bd1680187923
Sejun Kim <kimsejun@outlook.kr>
2022-11-28 15:57:33 +0900

    add vmextenctions

--- script/enable-hyperv.ps1
+++ script/enable-hyperv.ps1
@@ -1,14 +1,11 @@
 Install-WindowsFeature -Name hyper-v -IncludeAllSubFeature -IncludeManagementTools -Restart
 
-Invoke-WebRequest -Uri 
+New-Item -Path "C:\" -Name Temp -ItemType directory
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/kimsejun2000/maas_handson/main/script/set-environmentvhdx.ps1" -OutFile "C:\Temp\set-environmentvhdx.ps1"
 
-$action = New-ScheduledTaskAction -Execute "powershell -ExecutionPolicy Unrestricted"
+$action = New-ScheduledTaskAction -Execute "powershell -ExecutionPolicy Unrestricted -file C:\Temp\set-environmentvhdx.ps1"
 $trigger = New-ScheduledTaskTrigger -AtLogon
 $principal = "ubucon"
 $settings = New-ScheduledTaskSettingsSet
 $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
 Register-ScheduledTask T1 -InputObject $task
-
-
-git config --global user.email "you@example.com"
-git config --global user.name "Sejun Kim"
