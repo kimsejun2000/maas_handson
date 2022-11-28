70d2e8264805297685c6c9ee2f90205b42c23168
Sejun Kim <kimsejun@outlook.kr>
2022-11-28 16:49:12 +0900

    fixed scripts

--- script/enable-hyperv.ps1
+++ script/enable-hyperv.ps1
@@ -2,9 +2,8 @@ New-Item -Path "C:\" -Name Temp -ItemType directory
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/kimsejun2000/maas_handson/main/script/set-environmentvhdx.ps1" -OutFile "C:\Temp\set-environmentvhdx.ps1"
 
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Unrestricted -file C:\Temp\set-environmentvhdx.ps1"
-$delayTime = new-timespan -Minutes 1
-$trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay $delayTime
-$principal = New-ScheduledTaskPrincipal -UserId "ubucon" -RunLevel Highest
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$principal = New-ScheduledTaskPrincipal -UserId "S-1-5-21-1980097976-1178934822-2883624732-500" -LogonType S4U -RunLevel Highest
 $settings = New-ScheduledTaskSettingsSet
 $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
 Register-ScheduledTask initenv -InputObject $task
