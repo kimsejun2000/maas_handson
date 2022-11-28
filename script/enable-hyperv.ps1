336b4d8657628e02cdcfd2ee1b1e0ac6f8d3a742
Sejun Kim <kimsejun@outlook.kr>
2022-11-28 20:58:33 +0900

    fixed sceipt

--- script/enable-hyperv.ps1
+++ script/enable-hyperv.ps1
@@ -3,7 +3,7 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/kimsejun2000/maas_hand
 
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Unrestricted -file C:\Temp\set-environmentvhdx.ps1"
 $trigger = New-ScheduledTaskTrigger -AtStartup
-$principal = New-ScheduledTaskPrincipal -UserId "S-1-5-21-1980097976-1178934822-2883624732-500" -LogonType S4U -RunLevel Highest
+$principal = New-ScheduledTaskPrincipal -UserId "S-1-5-18" -RunLevel Highest
 $settings = New-ScheduledTaskSettingsSet
 $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
 Register-ScheduledTask initenv -InputObject $task
